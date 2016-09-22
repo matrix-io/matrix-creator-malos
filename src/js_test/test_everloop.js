126dccb54223dc66a216ce9b07784628f036ae5e
Andres Calderon <andres.calderon@admobilize.com>
2016-09-22 21:00:52 +0000

    proto file location has been updated

--- src/js_test/test_everloop.js
+++ src/js_test/test_everloop.js
@@ -13,7 +13,7 @@ var creator_ip = '127.0.0.1'
 var creator_everloop_base_port = 20013 + 8 // port for Everloop driver.
 
 var protoBuf = require("protobufjs");
-var protoBuilder = protoBuf.loadProtoFile('../../protos/malos.proto')
+var protoBuilder = protoBuf.loadProtoFile('../../protocol-buffers/malos/driver.proto')
 var matrixMalosBuilder = protoBuilder.build("matrix_malos")
 
 var zmq = require('zmq')
