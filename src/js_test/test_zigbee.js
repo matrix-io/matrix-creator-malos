5b8e2c8d4b2abdd9e20705b3e8a61099302eb50c
Nelson Castillo <nelson.castillo@admobilize.com>
2016-09-23 21:03:36 +0000

    Improve sample for zigbee.

--- src/js_test/test_zigbee.js
+++ src/js_test/test_zigbee.js
@@ -1,3 +1,5 @@
+// NOTE: This file could be better. We know.
+
 // This is how we connect to the creator. IP and port.
 // The IP is the IP I'm using and you need to edit it.
 // By default, MALOS has its 0MQ ports open to the world.
@@ -48,7 +50,6 @@ updateSocket.on('message', function(buffer) {
 
   var bulbCmd = new matrixMalosBuilder.ZigBeeBulbCmd
 
-  // { short_id: 0, command: 0, params: [] }
   bulbCmd.short_id = data.short_id 
   bulbCmd.command = matrixMalosBuilder.ZigBeeBulbCmd.EnumCommands.OFF
   console.log(bulbCmd)
@@ -66,44 +67,4 @@ updateSocket.on('message', function(buffer) {
     console.log('sending toggle')
     configSocket.send(config.encode().toBuffer());
   }, 2000);
-
-  //var bulbCmd = matrixMalosBuilder.build('ZigBeeBulbCmd')
-  //var bulCmdOff = new bulbCmd('ZigBeeBulbCmd.EnumCommands.OFF')
-  //console.log(bulCmdOff)
-
-  /*
-  bulbCommands = new matrixMalosBuilder.ZigBeeBulbCmd
-  var command_OFF = new bulbCommands
-  console.log(bulbCommands)
-  */
 });
-
-
-
-/*
-function setEverloop() {
-    var config = new matrixmalosbuilder.driverconfig
-    config.image = new matrixmalosbuilder.everloopimage
-    for (var j = 0; j < 35; ++j) {
-      var ledValue = new matrixMalosBuilder.LedValue;
-      ledValue.setRed(0);
-      ledValue.setGreen(intensity_value);
-      ledValue.setBlue(0);
-      ledValue.setWhite(0);
-      config.image.led.push(ledValue)
-    }
-    configSocket.send(config.encode().toBuffer());
-}
-
-
-StEverloop(intensity_value)
-setInterval(function() {
-  intensity_value -= 1
-  if (intensity_value < 0)
-    intensity_value = max_intensity
-  setEverloop()
-}, 10);
-
-*/
-
-
