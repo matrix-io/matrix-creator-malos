dd959d4abe62d8f7f2587d7cffd474a317c7a814
Antonio Vanegas <hpsaturn@gmail.com>
2016-09-28 15:30:36 -0500

    mockup for LIRC support

---
+++ src/driver_lirc.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 <Admobilize>
+ * MATRIX Labs  [http://creator.matrix.one]
+ * This file is part of MATRIX Creator MALOS
+ *
+ * MATRIX Creator MALOS is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+#include "./driver_lirc.h"
+
+#include "./src/driver.pb.h"
+#include "matrix_hal/lirc.h"
+
+namespace matrix_malos {
+
+bool LircDriver::ProcessConfig(const DriverConfig& config) {
+  LircParams lirc(config.lirc());
+
+  if (lirc.device() == "" || lirc.command() == "") {
+    std::string error_msg("device or command parameter is missing");
+    error_msg += kLircDriverName;
+    zmq_push_error_->Send(error_msg);
+    return false;
+  }
+
+  return true;
+
+}
+
+}  // namespace matrix_malos
---
+++ src/driver_lirc.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016 <Admobilize>
+ * MATRIX Labs  [http://creator.matrix.one]
+ * This file is part of MATRIX Creator MALOS
+ *
+ * MATRIX Creator MALOS is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SRC_DRIVER_LIRC_H_
+#define SRC_DRIVER_LIRC_H_
+
+const char kLircDriverName[] = "Lirc";
+
+namespace matrix_malos {
+
+class LircDriver : public MalosBase {
+ public:
+  LircDriver() : MalosBase(kLircDriverName) {
+    SetProvidesUpdates(false);
+    SetNeedsKeepalives(false);
+    SetNotesForHuman(
+        "Write-read. Get list remotes and send commands via IR. In development"
+    );
+  }
+
+  // Receive a copy of the shared wishbone bus. Not owned.
+  void SetupBus(matrix_hal::WishboneBus* wishbone) override {
+    writer_.reset(new matrix_hal::Everloop);
+    writer_->Setup(wishbone);
+  }
+
+  // Read configuration of LEDs (from the outside world).
+  bool ProcessConfig(const DriverConfig& config) override;
+
+ private:
+  // Everloop writer.
+  std::unique_ptr<matrix_hal::Everloop> writer_;
+};
+
+}  // namespace matrix_malos
+
+#endif  // SRC_DRIVER_EVERLOOP_H_
