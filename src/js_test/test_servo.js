3d4be96cb5e53ca1a52d6272c9abab8648942385
Antonio Vanegas <hpsaturn@gmail.com>
2016-10-10 18:42:18 -0500

    basic nodejs test for servo driver

--- src/js_test/test_servo.js
+++ src/js_test/test_servo.js
@@ -9,7 +9,7 @@
 // BasePort + 2 => Error port. Receive errros from device.
 // BasePort + 3 => Data port. Receive data from device.
 
-var creator_ip = '127.0.0.1'
+var creator_ip = '192.168.2.128'
 var creator_everloop_base_port = 20013 + 32 // port for Everloop driver.
 
 var protoBuf = require("protobufjs");
@@ -20,13 +20,17 @@ var zmq = require('zmq')
 var configSocket = zmq.socket('push')
 configSocket.connect('tcp://' + creator_ip + ':' + creator_everloop_base_port /* config */)
 
-function sendServoCommand() {
-  var servo_cfg_cmd = new matrixMalosBuilder.ServoParams
-  servo_cfg_cmd.set_pin(4)
-  servo_cfg_cmd.set_angle(30)
+var count=0
 
-  var config = new matrixMalosBuilder.DriverConfig
-  config.set_servo(servo_cfg_cmd)
+function sendServoCommand() {
+  var servo_cfg_cmd = new matrixMalosBuilder.ServoParams;
+  servo_cfg_cmd.set_pin(4);
+
+  process.nextTick(function() {count=count+10});
+  servo_cfg_cmd.set_angle(count%180)
+
+  var config = new matrixMalosBuilder.DriverConfig;
+  config.set_servo(servo_cfg_cmd);
   configSocket.send(config.encode().toBuffer());
 }
 
