5f5d2bf5718a12c228bf7122a3df793af1ca85bb
Antonio Vanegas <hpsaturn@gmail.com>
2016-11-01 21:14:18 -0500

    add basic gpio driver, input/output support only

---
+++ src/driver_gpio.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016 <Admobilize>
+ * MATRIX Labs  [http://creator.matrix.one]
+ * This file is part of MATRIX Creator MALOS
+ *
+ * MATRIX Creator MALOS is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+#include "./driver_gpio.h"
+#include "./src/driver.pb.h"
+
+namespace matrix_malos {
+
+const bool kGpioDriverDebugEnabled = false;
+
+bool GpioDriver::ProcessConfig(const DriverConfig& config) {
+  GpioParams gpio_config(config.gpio());
+  int16_t pin = (int16_t)gpio_config.pin();
+  int16_t mode = (int16_t)gpio_config.mode();
+
+  gpio_->SetMode(pin,mode);
+
+  if (mode == GpioParams::INPUT){
+    GpioParams gpiopb;
+    gpiopb.set_value(gpio_->GetGPIOValue(pin));
+    std::string buffer;
+    gpiopb.SerializeToString(&buffer);
+    zqm_push_update_->Send(buffer);
+  }
+  else if (mode == GpioParams::OUTPUT){
+    int16_t value = gpio_config.value();
+    gpio_->SetGPIOValue(pin,value);
+  }
+  else {
+    zmq_push_error_->Send(
+          "invalid gpio mode. check the proto GpioParams (file "
+          "driver.proto)");
+      return false;
+  }
+
+  return true;
+}
+
+}  // namespace matrix_malos
---
+++ src/driver_gpio.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 <Admobilize>
+ * MATRIX Labs  [http://creator.matrix.one]
+ * This file is part of MATRIX Creator MALOS
+ *
+ * MATRIX Creator MALOS is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SRC_DRIVER_GPIO_H_
+#define SRC_DRIVER_GPIO_H_
+
+#include "./malos_wishbone_base.h"
+#include "matrix_hal/wishbone_bus.h"
+#include "matrix_hal/gpio_control.h"
+
+const char kGpioDriverName[] = "Gpio";
+
+namespace matrix_malos {
+
+class GpioDriver : public MalosWishboneBase {
+ public:
+  GpioDriver() : MalosWishboneBase(kGpioDriverName) {
+    SetProvidesUpdates(false);
+    SetNeedsKeepalives(false);
+    SetNotesForHuman("Write-read. Gpio handler. In development");
+  }
+
+  // Receive a copy of the shared wishbone bus. Not owned.
+  void SetupWishboneBus(matrix_hal::WishboneBus* wishbone) override {
+    gpio_.reset(new matrix_hal::GPIOControl);
+    gpio_->Setup(wishbone);
+  }
+
+  // Read configuration of Gpio command
+  bool ProcessConfig(const DriverConfig& config) override;
+
+ private:
+  // GPIO control
+  std::unique_ptr<matrix_hal::GPIOControl> gpio_;
+};
+
+}  // namespace matrix_malos
+
+#endif  // SRC_DRIVER_GPIO_H_
