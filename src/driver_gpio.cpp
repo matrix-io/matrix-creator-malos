c510b99ad9564bfbb34c35c353849140cea50010
Antonio Vanegas <hpsaturn@gmail.com>
2016-11-21 15:16:13 -0500

    add update override funcition. Return GPIO vector

--- src/driver_gpio.cpp
+++ src/driver_gpio.cpp
@@ -30,8 +30,16 @@ bool GpioDriver::ProcessConfig(const DriverConfig& config) {
   const int16_t pin = static_cast<int16_t>(gpio_config.pin());
   const int16_t mode = static_cast<int16_t>(gpio_config.mode());
 
-  if (mode == GpioParams::OUTPUT) {
+  if (mode == GpioParams::INPUT) {
+    GpioParams gpiopb;
+    gpiopb.set_value(gpio_->GetGPIOValue(pin));
+    std::string buffer;
+    gpiopb.SerializeToString(&buffer);
+    zqm_push_update_->Send(buffer);
+
+  } else if (mode == GpioParams::OUTPUT) {
     gpio_->SetGPIOValue(pin, gpio_config.value());
+
   } else if (!(mode == GpioParams::OUTPUT || mode == GpioParams::INPUT)) {
     zmq_push_error_->Send(
         "invalid gpio mode. check the proto GpioParams (file "
@@ -46,7 +54,7 @@ bool GpioDriver::ProcessConfig(const DriverConfig& config) {
 
 bool GpioDriver::SendUpdate() {
   GpioParams gpiopb;
-  gpiopb.set_value(gpio_->GetGPIOValue(pin));
+  gpiopb.set_vector(gpio_->GetGPIOVector());
   std::string buffer;
   gpiopb.SerializeToString(&buffer);
   zqm_push_update_->Send(buffer);
