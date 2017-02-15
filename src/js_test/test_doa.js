16c94b395dcdfaec2880bffeae279aab8c283826
Andres Calderon <andres.calderon@admobilize.com>
2017-02-15 20:02:20 +0000

    adding example of direction of arrival

---
+++ src/js_test/test_doa.js
@@ -0,0 +1,70 @@
+// Warning! This is returning 0's.
+// Missing low level logic. We're on it.
+
+// This is how we connect to the creator. IP and port.
+// The IP is the IP I'm using and you need to edit it.
+// By default, MALOS has its 0MQ ports open to the world.
+
+// Every device is identified by a base port. Then the mapping works
+// as follows:
+// BasePort     => Configuration port. Used to config the device.
+// BasePort + 1 => Keepalive port. Send pings to this port.
+// BasePort + 2 => Error port. Receive errros from device.
+// BasePort + 3 => Data port. Receive data from device.
+
+var creator_ip = '127.0.0.1'
+var creator_micarray_base_port = 20037
+
+var protoBuf = require("protobufjs")
+
+
+// Parse proto file
+var protoBuilder = protoBuf.loadProtoFile('../../protocol-buffers/malos/driver.proto')
+// Parse matrix_malos package (namespace).
+var matrixMalosBuilder = protoBuilder.build("matrix_malos")
+
+var zmq = require('zmq')
+
+// ********** Start error management.
+var errorSocket = zmq.socket('sub')
+errorSocket.connect('tcp://' + creator_ip + ':' + (creator_micarray_base_port + 2))
+errorSocket.subscribe('')
+errorSocket.on('message', function(error_message) {
+  process.stdout.write('Message received: UV error: ' + error_message.toString('utf8') + "\n")
+});
+// ********** End error management.
+
+
+// ********** Start configuration.
+var configSocket = zmq.socket('push')
+configSocket.connect('tcp://' + creator_ip + ':' + creator_micarray_base_port)
+// Send driver configuration.
+var driverConfigProto = new matrixMalosBuilder.DriverConfig
+// 1 seconds between updates.
+driverConfigProto.delay_between_updates = 1.0
+// Stop sending updates 6 seconds after pings.
+driverConfigProto.timeout_after_last_ping = 6.0
+configSocket.send(driverConfigProto.encode().toBuffer())
+// ********** End configuration.
+
+// ********** Start updates - Here is where they are received.
+var updateSocket = zmq.socket('sub')
+updateSocket.connect('tcp://' + creator_ip + ':' + (creator_micarray_base_port + 3))
+updateSocket.subscribe('')
+updateSocket.on('message', function(buffer) {
+  var data = new matrixMalosBuilder.MicArrayParams.decode(buffer)
+   console.log(data)
+  console.log("\nazimutal_angle (degrees) = " + data.azimutal_angle*180.0/Math.PI);
+  console.log("   polar_angle (degrees) = " + data.polar_angle*180.0/Math.PI); 
+});
+// ********** End updates
+
+// ********** Ping the driver
+var pingSocket = zmq.socket('push')
+pingSocket.connect('tcp://' + creator_ip + ':' + (creator_micarray_base_port + 1))
+process.stdout.write("Sending pings every 5 seconds");
+pingSocket.send(''); // Ping the first time.
+setInterval(function(){
+  pingSocket.send('');
+}, 5000);
+// ********** Ping the driver ends
